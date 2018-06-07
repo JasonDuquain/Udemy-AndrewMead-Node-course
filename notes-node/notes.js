5c08e1d8478dc8cb13219fe3f5f54e04b68759c6
jasonduquain <jduquain@gmail.com>
2018-06-07 15:06:32 -0400

    refactoring for usability complete

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -2,8 +2,36 @@
 const fs = require('fs');
 
 
+var fetchNotes = () => {
+    try {
+        // the below 2 lines ensure new entries get appended as opposed to having existing lines removed and just the latest node added
+        var notesString = fs.readFileSync('notes-data.json'); // put this is the try blk in case the json file does not exist yet
+        return JSON.parse(notesString);
+    } catch(e) {
+        return [];
+    }   
+};
+
+var saveNotes = (notes) => {
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
 var addNote = (title, body) => {
-    console.log('adding note', title, body);
+    var notes = fetchNotes();
+    var note = {
+        title: title,
+        body: body
+    };
+    
+    // if duplicateNotes arr has any items (if the filter returns true) then the note already exists and it should not be added
+    var duplicateNotes = notes.filter((note) => note.title === title);
+    
+    if (duplicateNotes.length === 0) {
+        notes.push(note);
+        saveNotes(notes);
+        return note;
+    } 
+    
 };
 
 var getAll = () => {
